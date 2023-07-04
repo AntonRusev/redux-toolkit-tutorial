185d1e5f28a5c3f3f16b6e1764263146e3827d82
AntonRusev <anton_r@abv.bg>
2023-07-04 15:59:18 +0300

    implemented Counter with redux-toolkit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Counter from './features/counter/Counter';
+
+import './App.css';
+
+function App() {
+
+    return (
+        <>
+            <h1>Redux Toolkit Tutorial</h1>
+            <Counter />
+        </>
+    );
+};
+
+export default App;
