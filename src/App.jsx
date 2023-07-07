ebdd8b4039b13fb062b92d3c2dc613fc04e378ce
AntonRusev <anton_r@abv.bg>
2023-07-07 15:17:28 +0300

    implemented CRUD functionality for Posts

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,37 @@
 import Counter from './features/counter/Counter';
 
 import './App.css';
+
 import PostsList from './features/posts/PostsList';
 import AddPostForm from './features/posts/AddPostForm';
+import SinglePostPage from './features/posts/SinglePostPage';
+import EditPostForm from './features/posts/EditPostForm';
+
+import Layout from './components/Layout';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
 
     return (
         <>
-            <h1>Redux Toolkit Tutorial</h1>
-            {/* <Counter /> */}
+            <Routes>
+                <Route patch="/" element={<Layout />}>
+
+                    <Route index element={<PostsList />} />
+
+                    <Route path="post">
+                        <Route index element={<AddPostForm />} />
+                        <Route path=":postId" element={<SinglePostPage />} />
+                        <Route path="edit/:postId" element={<EditPostForm />} />
+                    </Route>
+
+                </Route>
+            </Routes>
+
+            {/* <h1>Redux Toolkit Tutorial</h1>
+            <Counter />
             <AddPostForm />
-            <PostsList />
+            <PostsList /> */}
         </>
     );
 };
--- src/features/posts/PostsExcerpt.jsx
+++ src/features/posts/PostsExcerpt.jsx
@@ -2,14 +2,20 @@ import PostAuthor from "./PostAuthor";
 import TimeAgo from "./TimeAgo";
 import ReactionButtons from "./ReactionButtons";
 
+import { Link } from 'react-router-dom';
+
 const PostsExcerpt = ({ post }) => {
     return (
         <article>
-            <h3>{post.title}</h3>
+            <h2>{post.title}</h2>
 
-            <p>{post.body.substring(0, 100)}</p>
+            <p>{post.body.substring(0, 75)}...</p>
 
             <p>
+                <Link to={`post/${post.id}`}>
+                    View Post
+                </Link>
+
                 <PostAuthor userId={post.userId} />
                 <TimeAgo timestamp={post.date} />
             </p>
